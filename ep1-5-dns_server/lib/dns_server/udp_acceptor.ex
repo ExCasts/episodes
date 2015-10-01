977b7f95f266025805685314201e68f9bb1c375a
Jason Harrelson <jason@excasts.com>
2015-09-30 23:52:37 -0500

    Add UdpAcceptor initialization.

--- ep1-5-dns_server/lib/dns_server/udp_acceptor.ex
+++ ep1-5-dns_server/lib/dns_server/udp_acceptor.ex
@@ -1,3 +1,25 @@
 defmodule DnsServer.UdpAcceptor do
 
+  use GenServer
+
+  require Logger
+
+  # Public ##########
+
+  def start_link(args \\ [], opts \\ []) do
+    GenServer.start_link(__MODULE__, args, opts)
+  end
+
+  # Private ##########
+
+  def init(args) do
+    {:ok, port} = Keyword.fetch(List.wrap(args), :port)
+
+    {:ok, socket} = :gen_udp.open(port, [:binary, active: true, reuseaddr: true])
+
+    Logger.info "Accepting UDP connections on port #{port}"
+
+    {:ok, %{port: port, socket: socket}}
+  end
+
 end
