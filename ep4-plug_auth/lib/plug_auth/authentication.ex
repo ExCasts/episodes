45152903011d2ff5721a66213c18377b186ccee5
Jason Harrelson <jason@excasts.com>
2015-10-22 22:39:15 -0500

    Refactor auth plug to actually look for correct token.

--- ep4-plug_auth/lib/plug_auth/authentication.ex
+++ ep4-plug_auth/lib/plug_auth/authentication.ex
@@ -2,16 +2,35 @@ defmodule PlugLogger.Authentication do
 
   import Plug.Conn
 
-  import Plug.Conn,          only: [put_status: 2, halt: 1]
+  import Plug.Conn,          only: [get_req_header: 2, put_status: 2, halt: 1]
   import Phoenix.Controller, only: [json: 2]
 
   def init( options ), do: options
 
   def call( conn, _options ) do
-    conn
-    |> put_status( :unauthorized )
-    |> json( %{error: %{message: "Authentication failure"}} )
-    |> halt
+    case authorize( conn ) do
+      :authorized ->
+        conn
+      :unauthorized ->
+        conn
+        |> put_status( :unauthorized )
+        |> json( %{error: %{message: "Authentication failure"}} )
+        |> halt
+    end
+  end
+
+  defp authorize( conn ) do
+    provided_token = get_req_header( conn, "auth-token" ) |> List.first
+
+    if provided_token == token, do: :authorized, else: :unauthorized
+    #cond do
+      #provided_token == token -> :authorized
+      #true -> :unatuhorized
+    #end
+  end
+
+  defp token do
+    "123456789"
   end
 
 end
