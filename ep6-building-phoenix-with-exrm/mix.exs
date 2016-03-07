55d9fd9286b20fec01d56b7838516346810ad99b
Jason Harrelson <cjharrelson@Amy-MBPro13.local>
2016-03-06 19:08:16 -0600

    Install Exrm.

--- ep6-building-phoenix-with-exrm/mix.exs
+++ ep6-building-phoenix-with-exrm/mix.exs
@@ -3,7 +3,7 @@ defmodule Hello.Mixfile do
 
   def project do
     [app: :hello,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
@@ -35,7 +35,8 @@ defmodule Hello.Mixfile do
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:exrm, "~> 1.0"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
